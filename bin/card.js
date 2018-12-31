88334f39607e235cf9c2c226042b5ed9e76c2e5b
LashaunnaS <shaunna191@icloud.com>
2018-12-30 21:58:03 -0500

    colour update

--- bin/card.js
+++ bin/card.js
@@ -12,13 +12,13 @@ const options = {
   borderStyle: "round",
 };
 
-const cardText = chalk`{hex('#dfe5db') Lashaunna Samuels /} {hex('#87f1ff') @}
+const cardText = chalk`{hex('#fefeff') Lashaunna Samuels /} {hex('#87f1ff') @}
 
-  {hex('#dfe5db').bold Work:} {hex('#87f1ff') Curious Web Engineer}
-  {hex('#dfe5db').bold NPM:} {hex('#87f1ff') https://www.npmjs.com/~lashaunna191}
-  {hex('#dfe5db').bold Stack:} {hex('#87f1ff') https://stackoverflow.com/users/10176472/LashaunnaS}
-  {hex('#dfe5db').bold Github:} {hex('#87f1ff') https://github.com/LashaunnaS}
-  {hex('#dfe5db').bold LinkedIn:} {hex('#87f1ff') https://www.linkedin.com/in/lashaunna-samuels/}
+  {hex('#fefeff').bold Work:} {hex('#87f1ff') Curious Web Engineer}
+  {hex('#fefeff').bold NPM:} {hex('#87f1ff') https://www.npmjs.com/~lashaunna191}
+  {hex('#fefeff').bold Stack:} {hex('#87f1ff') https://stackoverflow.com/users/10176472/LashaunnaS}
+  {hex('#fefeff').bold Github:} {hex('#87f1ff') https://github.com/LashaunnaS}
+  {hex('#fefeff').bold LinkedIn:} {hex('#87f1ff') https://www.linkedin.com/in/lashaunna-samuels/}
 
   {hex('#dfe5db').bold Card:} {hex('#87f1ff') npx Lashaunna}`;
 
